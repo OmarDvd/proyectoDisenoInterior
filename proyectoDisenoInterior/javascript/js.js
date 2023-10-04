45078340dc93f8811ca4fef0d22ad0b1c0107800
judiith-mf <131663444+judiith-mf@users.noreply.github.com>
2023-10-04 08:31:46 +0200

    Logo cambiado y navegacion. Galeria cambiada

---
+++ proyectoDisenoInterior/javascript/js.js
@@ -0,0 +1,28 @@
+let boton1 = document.getElementById('boton1');
+let texto1 = document.getElementById('text1');
+
+let boton2 = document.getElementById('boton_blanco');
+let texto2 = document.getElementById('text2');
+
+let boton3 = document.getElementById('boton3');
+let texto3 = document.getElementById('text3');
+
+window.onload = function (){
+    boton1.onclick = seguirLey1;
+    
+    boton2.onclick = seguirLey2;
+    
+    boton3.onclick = seguirLey3;
+}
+
+function seguirLey1() {
+    texto1.style.display="block";
+}
+
+function seguirLey2() {
+    texto2.style.display="block";
+}
+
+function seguirLey3() {
+    texto3.style.display="block";
+}
